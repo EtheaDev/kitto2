faf0aa2e667f86195a2ad1e6d29b9edbab887847
nando <nandod@users.noreply.github.com>
2011-10-13 14:09:46 +0000

    * Small refactoring.

--- Home/Resources/js/kitto-core.js
+++ Home/Resources/js/kitto-core.js
@@ -1,47 +1,3 @@
-// defaultButton plugin allows to have a button that is automatically clicked
-// when the user presses Enter.
-(function(){
-    var ns = Ext.ns('Ext.ux.plugins');
-    /**
-     * @class Ext.ux.plugins.DefaultButton
-     * @extends Object
-     *
-     * Plugin for Button that will click() the button if the user presses ENTER while
-     * a component in the button's form has focus.
-     *
-     * @author Stephen Friedrich
-     * @date 21-JAN-2010
-     * @version 0.2
-     *
-     */
-    Ext.ux.plugins.DefaultButton =  Ext.extend(Object, {
-        init: function(button) {
-            button.on('afterRender', setupKeyListener, button);
-        }
-    });
-
-    function setupKeyListener() {
-        var formPanel = this.findParentByType('form');
-        //noinspection ObjectAllocationIgnored
-        new Ext.KeyMap(formPanel.el, {
-            key: Ext.EventObject.ENTER,
-            shift: false,
-            alt: false,
-            fn: function(keyCode, e){
-                if(this.hidden || e.target.type === 'textarea' && !e.ctrlKey) {
-                    return true;
-                }
-
-                this.el.select('button').item(0).dom.click();
-                return false;
-            },
-            scope: this
-        });
-    }
-
-    Ext.ComponentMgr.registerPlugin('defaultButton', ns.DefaultButton);
-})();  
-
 // Dynamically adds a style rule. Used to add icons
 // through CSS in async responses.
 function addStyleRule(rule) {
@@ -81,6 +37,8 @@ Ext.apply(Ext.form.VTypes, {
     alpha_spaceMask: /[a-z ]/i
 });
 
+// Fires change event if the object value is at least
+// minChars characters long. Used in filters.
 function fireChangeAfterNChars(obj, minChars)
 {
   var v = obj.getValue();
