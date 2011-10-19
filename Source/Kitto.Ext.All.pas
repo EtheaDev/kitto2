6df4ae22102277b77a71cdfe61b9669f9c3578ab
nando <nandod@users.noreply.github.com>
2011-10-19 07:12:22 +0000

    + Unit that includes all controllers.

---
+++ Source/Kitto.Ext.All.pas
@@ -0,0 +1,23 @@
+unit Kitto.Ext.All;
+
+interface
+
+uses
+  Kitto.Ext.AccordionPanel
+  , Kitto.Ext.BorderPanel
+  , Kitto.Ext.Filters
+  , Kitto.Ext.Form
+  , Kitto.Ext.HtmlPanel
+  , Kitto.Ext.List
+  , Kitto.Ext.Rules
+  , Kitto.Ext.StatusBar
+  , Kitto.Ext.TabPanel
+  , Kitto.Ext.ToolBar
+  , Kitto.Ext.TreePanel
+  , Kitto.Ext.Viewport
+  , Kitto.Ext.Window
+  ;
+
+implementation
+
+end.
