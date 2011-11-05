b146b756da058c01e4448b131e258d4498cd30c6
nando <nandod@users.noreply.github.com>
2011-11-05 11:23:31 +0000

    * Updated HelloKitto example moved inside Kitto.

---
+++ Examples/HelloKitto/Source/UseKitto.pas
@@ -0,0 +1,20 @@
+unit UseKitto;
+
+interface
+
+uses
+  // EF.DB.ADO,
+  EF.DB.DBX,
+  // Kitto.AccessControl.DB,
+  Kitto.Auth.DB,
+  // Kitto.Auth.DBServer,
+  // Kitto.Auth.OSDB,
+  // Kitto.Auth.TextFile,
+  Kitto.Localization.dxgettext,
+  Kitto.Metadata.ViewBuilders,
+  Kitto.Ext.All
+  ;
+
+implementation
+
+end.
