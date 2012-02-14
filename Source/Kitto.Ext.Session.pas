a9cf3e1642136c3ecbc16a50bc148c9fac95cce3
nando <nandod@users.noreply.github.com>
2012-02-14 16:30:24 +0000

    * Localization WIP. * Copyright note updated.

--- Source/Kitto.Ext.Session.pas
+++ Source/Kitto.Ext.Session.pas
@@ -1,5 +1,5 @@
 ﻿{-------------------------------------------------------------------------------
-   Copyright 2011 Ethea S.r.l.
+   Copyright 2012 Ethea S.r.l.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
