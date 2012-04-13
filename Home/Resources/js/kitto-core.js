9965846b37ab94a3eadb330d9d95d9f1175a92f1
nando <nandod@users.noreply.github.com>
2012-04-13 09:48:26 +0000

    * Moved startup executable stuff from kitto-core.js to kitto-init.js; only callable stuff left in kitto-core.js.

--- Home/Resources/js/kitto-core.js
+++ Home/Resources/js/kitto-core.js
@@ -12,44 +12,6 @@ function addStyleRule(rule) {
   head.appendChild(style);
 };
 
-// Used by TKExtForceCamelCaps.
-String.prototype.capitalize = function(){
-   return this.replace( /(^|\s)([a-z])/g , function(m,p1,p2){ return p1+p2.toUpperCase(); } );
-};
-
-// Additional form vtypes.
-var
-  codice_fiscale_re = /^[a-zA-Z]{6}[0-9]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9]{2}([a-zA-Z]{1}[0-9]{3})[a-zA-Z]{1}$/,
-  partita_iva_re = /^[0-9]{11}$/,
-  alpha_space_re = /^[a-zA-Z_ ]+$/;
-  phone_number_re = /^\+[0-9]+\-[0-9]+$/
-Ext.apply(Ext.form.VTypes, {
-    codice_fiscale: function(val, field) {
-      return codice_fiscale_re.test(val) || partita_iva_re.test(val);
-    },
-    codice_fiscaleText: 'Codice Fiscale non formalmente corretto.',
-    codice_fiscaleMask: /[a-zA-Z0-9]/i,
-    
-    alpha_space: function(val, field) {
-      return alpha_space_re.test(val);
-    },
-    alpha_spaceText: 'This field only accepts letters and spaces.',
-    alpha_spaceMask: /[a-z ]/i,
-
-    phone_number: function(val, field) {
-      return phone_number_re.test(val);
-    },
-    phone_numberText: 'Invalid phone number.',
-    phone_numberMask: /[a-z0-9\-\+]/
-});
-
-// Additional formats and renderers.
-Ext.apply(Ext.util.Format, {
-  checkboxRenderer: function(val) {
-    return String.format('<div class="x-grid3-check-col{0}"></div>', val ? "-on" : '');
-  }
-});
-
 // Fires change event if the object value is at least
 // minChars characters long. Also fires the event when the
 // value is empty. Used in filters.
@@ -188,6 +150,11 @@ function matchValue(value, patterns)
   return result;
 };
 
+// Creates a style rule (see addStyleRule()) named after the
+// color obtained by matching the record's specified field
+// against the patterns (see matchValue()). The rule sets
+// the background color to that color. Used to color grid rows
+// according to field values.
 function getRowColorStyleRule(record, fieldName, patterns)
 {
   var color = matchValue(record.get(fieldName), patterns);
