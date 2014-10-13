0e6f93621fdbe99ec808da2e746c0efdb18c9d5c
carlo <carlo@c6421abf-fb66-e144-b6b4-7ec664d51aad>
2014-10-13 12:24:34 +0000

    KEmplyee example: A Kitto example project based on Firebird Employee Database.

---
+++ trunk/Examples/KEmployee/Source/UseKitto.pas
@@ -0,0 +1,21 @@
+unit UseKitto;
+
+interface
+
+uses
+  EF.DB.ADO,
+  EF.DB.DBX,
+  // Kitto.AccessControl.DB,
+  Kitto.Auth.DB,
+  // Kitto.Auth.DBServer,
+  // Kitto.Auth.OSDB,
+  // Kitto.Auth.TextFile,
+  Kitto.Localization.dxgettext,
+  Kitto.Metadata.ModelImplementation,
+  Kitto.Metadata.ViewBuilders,
+  Kitto.Ext.All
+  ;
+
+implementation
+
+end.
