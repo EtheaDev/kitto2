0f9a596578375000bd542f5e07775968a133877e
Carlo Barazzetta <carlo.barazzetta@ethea.it>
2014-11-13 16:28:27 +0000

    Refactoring: created TExportFileToolController as base controller for files. create TExportTextToolController as base controller to export generic text file. changed TExportCSVToolController as inherited from TExportTextToolController

--- Source/Kitto.Ext.Tools.pas
+++ Source/Kitto.Ext.Tools.pas
@@ -24,77 +24,252 @@ uses
   Kitto.Metadata.DataView, Kitto.Ext.StandardControllers;
 
 type
-  TExportCSVToolController = class(TKExtDataToolController)
+  TExportFileToolController = class(TKExtDataToolController)
   private
-    FCSVData: string;
-    function StoreToCSV(const AStore: TKViewTableStore;
-      const ADelimiter: Char = ';'; const AQuoteChar: Char = '"'): string;
   protected
-    procedure ExecuteTool; override;
+    function ValidColumnName(const FieldName: string): string; virtual;
+    function GetOutputFileName: string; virtual;
+    function GetFileExtension: string;
+    function GetDefaultFileExtension: string; virtual;
   published
-    procedure DownloadFile;
+    property OutputFileName: string read GetOutputFileName;
+  end;
+
+  TExportTextToolController = class(TExportFileToolController)
+  private
+    function StoreToTextFile(const AStore: TKViewTableStore;
+      const AFixedLength: boolean;
+      const ADelimiter: Char; const AQuoteChar: Char;
+      const AIncludeHeader: boolean): string;
+    function GetDelimiter: char;
+    function GetFixedLength: boolean;
+    function GetIncludeHeader: boolean;
+    function GetQuoteChar: char;
+  protected
+    FTextContent: string;
+    procedure ExecuteTool; override;
+    function GetDefaultFileExtension: string; override;
+    function GetDefaultIncludeHeader: boolean; virtual;
+    function GetDefaultFixedLength: boolean; virtual;
+    function GetDefaultDelimiter: char; virtual;
+    function GetDefaultQuoteChar: char; virtual;
+  published
+    procedure DownloadTextFile;
+    property IncludeHeader: boolean read GetIncludeHeader;
+    property FixedLength: boolean read GetFixedLength;
+    property Delimiter: char read GetDelimiter;
+    property QuoteChar: char read GetQuoteChar;
+  end;
+
+  TExportCSVToolController = class(TExportTextToolController)
+  private
+  protected
+    function GetDefaultFileExtension: string; override;
+    function GetDefaultIncludeHeader: boolean; override;
+    function GetDefaultFixedLength: boolean; override;
   end;
 
 implementation
 
 uses
-  Ext, EF.DB,
+  StrUtils,
+  Ext, EF.DB, EF.Tree,
   Kitto.Ext.Session, Kitto.Config;
 
 { TExportCSVToolController }
 
-procedure TExportCSVToolController.DownloadFile;
-var
-  LCSVDataStream: TStringStream;
+function TExportCSVToolController.GetDefaultFixedLength: boolean;
 begin
-  LCSVDataStream := TStringStream.Create(FCSVData);
+  Result := False;
+end;
+
+function TExportCSVToolController.GetDefaultIncludeHeader: boolean;
+begin
+  Result := True;
+end;
+
+function TExportCSVToolController.GetDefaultFileExtension: string;
+begin
+  Result := '.csv';
+end;
+
+{ TExportFileToolController }
+
+function TExportFileToolController.GetDefaultFileExtension: string;
+begin
+  Result := '.txt';
+end;
+
+function TExportFileToolController.GetFileExtension: string;
+begin
+  Result := ExtractFileExt(OutputFileName);
+end;
+
+function TExportFileToolController.GetOutputFileName: string;
+var
+  LNode: TEFNode;
+begin
+  //Check specific OutputFileName node
+  LNode := Config.FindNode('OutputFileName');
+  if Assigned(LNode) then
+    Result := LNode.AsString
+  else
+    Result := ViewTable.PluralDisplayLabel + GetDefaultFileExtension;
+end;
+
+function TExportFileToolController.ValidColumnName(const FieldName: string): string;
+begin
+  Result := StringReplace(FieldName, ' ','_',[rfReplaceAll]);
+  Result := StringReplace(Result, '.','_',[rfReplaceAll]);
+end;
+
+{ TExportTextToolController }
+
+procedure TExportTextToolController.DownloadTextFile;
+var
+  LTextDataStream: TStringStream;
+begin
+  LTextDataStream := TStringStream.Create(FTextContent);
   try
-    LCSVDataStream.Seek(0, soFromBeginning);
-    Session.DownloadStream(LCSVDataStream, ViewTable.PluralDisplayLabel + '.csv');
+    LTextDataStream.Seek(0, soFromBeginning);
+    Session.DownloadStream(LTextDataStream, GetOutputFileName);
   finally
-    FreeAndNil(LCSVDataStream);
+    FreeAndNil(LTextDataStream);
   end;
 end;
 
-procedure TExportCSVToolController.ExecuteTool;
+procedure TExportTextToolController.ExecuteTool;
 begin
   inherited;
-  FCSVData := StoreToCSV(ServerStore);
-  Download(DownloadFile);
+  FTextContent := StoreToTextFile(ServerStore, FixedLength, Delimiter, QuoteChar, IncludeHeader);
+  Download(DownloadTextFile);
 end;
 
-function TExportCSVToolController.StoreToCSV(const AStore: TKViewTableStore;
-  const ADelimiter, AQuoteChar: Char): string;
+function TExportTextToolController.GetDefaultDelimiter: char;
+begin
+  Result := ';';
+end;
+
+function TExportTextToolController.GetDefaultFixedLength: boolean;
+begin
+  Result := True;
+end;
+
+function TExportTextToolController.GetDefaultIncludeHeader: boolean;
+begin
+  Result := False;
+end;
+
+function TExportTextToolController.GetDefaultQuoteChar: char;
+begin
+  Result := '"';
+end;
+
+function TExportTextToolController.GetDelimiter: char;
+var
+  LNode: TEfNode;
+begin
+  LNode := Config.FindNode('Delimiter');
+  if Assigned(LNode) then
+    Result := LNode.AsChar
+  else
+    Result := GetDefaultDelimiter;
+end;
+
+function TExportTextToolController.GetFixedLength: boolean;
+var
+  LNode: TEfNode;
+begin
+  LNode := Config.FindNode('FixedLength');
+  if Assigned(LNode) then
+    Result := LNode.AsBoolean
+  else
+    Result := GetDefaultFixedLength;
+end;
+
+function TExportTextToolController.GetIncludeHeader: boolean;
+var
+  LNode: TEfNode;
+begin
+  LNode := Config.FindNode('IncludeHeader');
+  if Assigned(LNode) then
+    Result := LNode.AsBoolean
+  else
+    Result := GetDefaultIncludeHeader;
+end;
+
+function TExportTextToolController.GetQuoteChar: char;
+var
+  LNode: TEfNode;
+begin
+  LNode := Config.FindNode('QuoteChar');
+  if Assigned(LNode) then
+    Result := LNode.AsChar
+  else
+    Result := GetDefaultQuoteChar;
+end;
+
+function TExportTextToolController.GetDefaultFileExtension: string;
+begin
+  Result := '.txt';
+end;
+
+function TExportTextToolController.StoreToTextFile(
+  const AStore: TKViewTableStore; const AFixedLength: boolean; const ADelimiter,
+  AQuoteChar: Char; const AIncludeHeader: boolean): string;
 var
   LRecordIndex: Integer;
+  LValue, LLine: string;
   LRecord: TKViewTableRecord;
   LFieldIndex: Integer;
   LField: TKViewTableField;
   LStringList: TStringList;
+  LViewField: TKViewField;
 
-  procedure AddCSVRow(const ARowData: string);
+  procedure AddRow(const ARowData: string);
   begin
     if Result <> '' then
       Result := Result + sLineBreak;
     Result := Result + ARowData;
   end;
 
+  function FormatLine(const ALine: string; const ASize: integer): string;
+  begin
+    Result := Copy(ALine,1,ASize)+StringOfChar(' ', ASize - Length(ALine));
+  end;
+
 begin
   Result := '';
   LStringList := TStringList.Create;
   try
-    LStringList.Delimiter := ADelimiter;
-    LStringList.QuoteChar := AQuoteChar;
-    LStringList.StrictDelimiter := True;
-
-    // Header.
-    for LFieldIndex := 0 to AStore.Header.FieldCount - 1 do
+    if not AFixedLength then
     begin
-      if Assigned(AStore.Header.Fields[LFieldIndex].ViewField) and
-          AStore.Header.Fields[LFieldIndex].ViewField.IsVisible then
-        LStringList.Add(AStore.Header.Fields[LFieldIndex].ViewField.DisplayLabel);
+      LStringList.Delimiter := ADelimiter;
+      LStringList.QuoteChar := AQuoteChar;
+      LStringList.StrictDelimiter := True;
     end;
-    AddCSVRow(LStringList.DelimitedText);
+
+    if AIncludeHeader then
+    begin
+      // Header.
+      LLine := '';
+      for LFieldIndex := 0 to AStore.Header.FieldCount - 1 do
+      begin
+        LViewField := AStore.Header.Fields[LFieldIndex].ViewField;
+        if Assigned(LViewField) then
+        begin
+          LValue := ValidColumnName(LViewField.DisplayLabel);
+          if AFixedLength then
+            LLine := LLine + FormatLine(LValue, LViewField.DisplayWidth)
+          else
+            LStringList.Add(LValue);
+        end;
+      end;
+      if AFixedLength then
+        AddRow(LLine)
+      else
+        AddRow(LStringList.DelimitedText);
+    end;
 
     // Rows.
     for LRecordIndex := 0 to AStore.RecordCount -1 do
@@ -103,13 +278,23 @@ begin
       LRecord := AStore.Records[LRecordIndex];
       if not LRecord.IsDeleted then
       begin
+        LLine := '';
         for LFieldIndex := 0 to LRecord.FieldCount - 1 do
         begin
           LField := LRecord.Fields[LFieldIndex];
-          if Assigned(LField.ViewField) and LField.ViewField.IsVisible then
-            LStringList.Add(LField.GetAsJSONValue(True, False, True));
+          if Assigned(LField.ViewField) then
+          begin
+            LValue := LField.GetAsJSONValue(True, False, True);
+            if AFixedLength then
+              LLine := LLine + FormatLine(LValue, LField.ViewField.DisplayWidth)
+            else
+              LStringList.Add(LValue);
+          end;
         end;
-        AddCSVRow(LStringList.DelimitedText);
+      if AFixedLength then
+        AddRow(LLine)
+      else
+        AddRow(LStringList.DelimitedText);
       end;
     end;
   finally
@@ -119,8 +304,10 @@ end;
 
 initialization
   TKExtControllerRegistry.Instance.RegisterClass('ExportCSVTool', TExportCSVToolController);
+  TKExtControllerRegistry.Instance.RegisterClass('ExportTextTool', TExportTextToolController);
 
 finalization
   TKExtControllerRegistry.Instance.UnregisterClass('ExportCSVTool');
+  TKExtControllerRegistry.Instance.UnregisterClass('ExportTextTool');
 
 end.
