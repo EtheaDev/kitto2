d1e586c2fa454e018b909b6e43221b35b7c24248
Carlo Barazzetta <carlo.barazzetta@ethea.it>
2014-11-14 19:47:59 +0000

    Taskitto: added example of PDF merge using Debenu Quick PDF library.

--- Examples/TasKitto/Source/UseKitto.pas
+++ Examples/TasKitto/Source/UseKitto.pas
@@ -11,6 +11,7 @@ uses
   // Kitto.Auth.OSDB,
   // Kitto.Auth.TextFile,
   Kitto.Ext.ADO.Tools, //For Excel export
+  Kitto.Ext.DebenuQuickPDF.Tools, //For PDF Merge
   Kitto.Localization.dxgettext,
   Kitto.Metadata.ModelImplementation,
   Kitto.Metadata.ViewBuilders,
