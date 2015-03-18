5dc70913f2f186a5f1349a05944af1ff20846d23
nando <nando@c6421abf-fb66-e144-b6b4-7ec664d51aad>
2015-03-18 16:25:24 +0000

    * DownloadFile method patched to be re-entrant due to multiple browser requests in some cases.

--- trunk/Source/Kitto.Ext.StandardControllers.pas
+++ trunk/Source/Kitto.Ext.StandardControllers.pas
@@ -25,114 +25,114 @@ uses
   Kitto.Ext.Base, Kitto.Ext.DataTool;
 
 type
-  ///	<summary>Logs the current user out ending the current session. Only
-  ///	useful if authentication is enabled.</summary>
+  /// <summary>Logs the current user out ending the current session. Only
+  /// useful if authentication is enabled.</summary>
   TKExtLogoutController = class(TKExtToolController)
   protected
     procedure ExecuteTool; override;
   public
-    ///	<summary>Returns the display label to use by default when not specified
-    ///	at the view or other level. Called through RTTI.</summary>
+    /// <summary>Returns the display label to use by default when not specified
+    /// at the view or other level. Called through RTTI.</summary>
     class function GetDefaultDisplayLabel: string;
 
-    ///	<summary>Returns the image name to use by default when not specified at
-    ///	the view or other level. Called through RTTI.</summary>
+    /// <summary>Returns the image name to use by default when not specified at
+    /// the view or other level. Called through RTTI.</summary>
     class function GetDefaultImageName: string;
   end;
 
 
-  ///	<summary>Base class for URL controllers.</summary>
+  /// <summary>Base class for URL controllers.</summary>
   TKExtURLControllerBase = class(TKExtDataToolController)
   protected
     function GetURL: string; virtual; abstract;
     procedure ExecuteTool; override;
   end;
 
-  ///	<summary>
-  ///	  <para>Navigates to a specified URL in a different browser
-  ///	  window/tab.</para>
-  ///	  <para>Params:</para>
-  ///	  <list type="table">
-  ///	    <listheader>
-  ///	      <term>Term</term>
-  ///	      <description>Description</description>
-  ///	    </listheader>
-  ///	    <item>
-  ///	      <term>TargetURL</term>
-  ///	      <description>URL to navigate to. May contain macros.</description>
-  ///	    </item>
-  ///	  </list>
-  ///	</summary>
+  /// <summary>
+  ///   <para>Navigates to a specified URL in a different browser
+  ///   window/tab.</para>
+  ///   <para>Params:</para>
+  ///   <list type="table">
+  ///     <listheader>
+  ///       <term>Term</term>
+  ///       <description>Description</description>
+  ///     </listheader>
+  ///     <item>
+  ///       <term>TargetURL</term>
+  ///       <description>URL to navigate to. May contain macros.</description>
+  ///     </item>
+  ///   </list>
+  /// </summary>
   TKExtURLController = class(TKExtURLControllerBase)
   protected
     function GetURL: string; override;
   end;
 
-  ///	<summary>
-  ///	  <para>Navigates to a specified URL in a different browser window/tab.
-  ///	  Several URLs can be specified and the choice is made by filtering on
-  ///	  request headers (for example, you can navigate to a different URL
-  ///	  depending on the client IP address or class of addresses).</para>
-  ///	  <para>Params:</para>
-  ///	  <list type="table">
-  ///	    <listheader>
-  ///	      <term>Term</term>
-  ///	      <description>Description</description>
-  ///	    </listheader>
-  ///	    <item>
-  ///	      <term>Filters</term>
-  ///	      <description>A collection of filters. Each node contains a Header
-  ///	      to filter upon (ex. REMOTE_ADDR), a Pattern to match the header
-  ///	      value to, and a TargetURL which may contain macros.</description>
-  ///	    </item>
-  ///	    <item>
-  ///	      <term>DefaultURL</term>
-  ///	      <description>Optional URL to navigate to when no filters
-  ///	      apply.</description>
-  ///	    </item>
-  ///	  </list>
-  ///	</summary>
-  ///	<remarks>If no filters apply, and DefaultURL is not specified, navigation
-  ///	is not performed.</remarks>
+  /// <summary>
+  ///   <para>Navigates to a specified URL in a different browser window/tab.
+  ///   Several URLs can be specified and the choice is made by filtering on
+  ///   request headers (for example, you can navigate to a different URL
+  ///   depending on the client IP address or class of addresses).</para>
+  ///   <para>Params:</para>
+  ///   <list type="table">
+  ///     <listheader>
+  ///       <term>Term</term>
+  ///       <description>Description</description>
+  ///     </listheader>
+  ///     <item>
+  ///       <term>Filters</term>
+  ///       <description>A collection of filters. Each node contains a Header
+  ///       to filter upon (ex. REMOTE_ADDR), a Pattern to match the header
+  ///       value to, and a TargetURL which may contain macros.</description>
+  ///     </item>
+  ///     <item>
+  ///       <term>DefaultURL</term>
+  ///       <description>Optional URL to navigate to when no filters
+  ///       apply.</description>
+  ///     </item>
+  ///   </list>
+  /// </summary>
+  /// <remarks>If no filters apply, and DefaultURL is not specified, navigation
+  /// is not performed.</remarks>
   TKExtFilteredURLController = class(TKExtURLControllerBase)
   protected
     function GetURL: string; override;
   end;
 
-  ///	<summary>Downloads a file that exists on disk or (by inheriting from it)
-  ///	is prepared on demand as a file or stream.</summary>
-  ///	<remarks>
-  ///	  <para>This class can be uses as-is to serve existing files, or
-  ///	  inherited to serve on-demand files and streams.</para>
-  ///	  <para>Params for the as-is version:</para>
-  ///	  <list type="table">
-  ///	    <listheader>
-  ///	      <term>Term</term>
-  ///	      <description>Description</description>
-  ///	    </listheader>
-  ///	    <item>
-  ///	      <term>FileName</term>
-  ///	      <description>Name of the file to serve (complete with full path).
-  ///	      May contain macros.</description>
-  ///	    </item>
-  ///	    <item>
-  ///	      <term>ClientFileName</term>
-  ///	      <description>File name as passed to the client; if not specified,
-  ///	      the name portion of FileName is used.</description>
-  ///	    </item>
-  ///	    <item>
-  ///	      <term>ContentType</term>
-  ///	      <description>Content type passed to the client; if not specified,
-  ///	      it is derived from the file name's extension.</description>
-  ///	    </item>
-  ///	    <item>
-  ///	      <term>PersistentFileName</term>
-  ///	      <description>Name of the file optionally persisted on the server
+  /// <summary>Downloads a file that exists on disk or (by inheriting from it)
+  /// is prepared on demand as a file or stream.</summary>
+  /// <remarks>
+  ///   <para>This class can be uses as-is to serve existing files, or
+  ///   inherited to serve on-demand files and streams.</para>
+  ///   <para>Params for the as-is version:</para>
+  ///   <list type="table">
+  ///     <listheader>
+  ///       <term>Term</term>
+  ///       <description>Description</description>
+  ///     </listheader>
+  ///     <item>
+  ///       <term>FileName</term>
+  ///       <description>Name of the file to serve (complete with full path).
+  ///       May contain macros.</description>
+  ///     </item>
+  ///     <item>
+  ///       <term>ClientFileName</term>
+  ///       <description>File name as passed to the client; if not specified,
+  ///       the name portion of FileName is used.</description>
+  ///     </item>
+  ///     <item>
+  ///       <term>ContentType</term>
+  ///       <description>Content type passed to the client; if not specified,
+  ///       it is derived from the file name's extension.</description>
+  ///     </item>
+  ///     <item>
+  ///       <term>PersistentFileName</term>
+  ///       <description>Name of the file optionally persisted on the server
   ///        before download. No files are left on the server if this parameter
   ///        is not specified.</description>
-  ///	    </item>
-  ///	  </list>
-  ///	</remarks>
+  ///     </item>
+  ///   </list>
+  /// </remarks>
   TKExtDownloadFileController = class(TKExtDataToolController)
   strict private
     FTempFileNames: TStrings;
@@ -152,23 +152,23 @@ type
     procedure AddTempFilename(const AFileName: string);
     procedure Cleanup;
   protected
-    ///	<summary>Override this method to provide a default file name if it's
-    ///	not specified in the config. If you are using streams, don't override
-    ///	this method.</summary>
+    /// <summary>Override this method to provide a default file name if it's
+    /// not specified in the config. If you are using streams, don't override
+    /// this method.</summary>
     function GetDefaultFileName: string; virtual;
 
-    ///	<summary>If you are creating a file on demand, do it in this method. If
-    ///	you are using streams, don't override this method and use CreateStream
-    ///	instead.</summary>
-    ///	<param name="AFileName">File name as read from the FileName param or
-    ///	returned by GetDefaultFileName.</param>
+    /// <summary>If you are creating a file on demand, do it in this method. If
+    /// you are using streams, don't override this method and use CreateStream
+    /// instead.</summary>
+    /// <param name="AFileName">File name as read from the FileName param or
+    /// returned by GetDefaultFileName.</param>
     procedure PrepareFile(const AFileName: string); virtual;
 
-    ///	<summary>Creates and returns a stream with the content to download.
-    ///	Override this method if you are using streams as opposed to
-    ///	files.</summary>
-    ///	<remarks>The caller will be responsible for freeing the stream when no
-    ///	longer needed.</remarks>
+    /// <summary>Creates and returns a stream with the content to download.
+    /// Override this method if you are using streams as opposed to
+    /// files.</summary>
+    /// <remarks>The caller will be responsible for freeing the stream when no
+    /// longer needed.</remarks>
     function CreateStream: TStream; virtual;
     procedure InitDefaults; override;
   public
@@ -247,8 +247,8 @@ procedure TKExtDownloadFileController.Cleanup;
 var
   I: Integer;
 begin
-  for I := 0 to FTempFileNames.Count-1 do
-    DeleteFile(FTempFileNames.Strings[I]);
+  for I := 0 to FTempFileNames.Count - 1 do
+    DeleteFile(FTempFileNames[I]);
   FTempFileNames.Clear;
 end;
 
@@ -333,14 +333,19 @@ var
   LStream: TStream;
 begin
   try
-    LStream := TFileStream.Create(FFileName, fmOpenRead);
-    try
-      DoDownloadStream(LStream, ClientFileName, ContentType);
-    finally
-      FreeAndNil(LStream);
+    // The file might not exist if the browser has sent multiple request and
+    // it was served before and then deleted (see Cleanup).
+    if FileExists(FFileName) then
+    begin
+      LStream := TFileStream.Create(FFileName, fmOpenRead);
+      try
+        DoDownloadStream(LStream, ClientFileName, ContentType);
+      finally
+        FreeAndNil(LStream);
+      end;
     end;
   finally
-    //Cleanup;
+    Cleanup;
   end;
 end;
 
@@ -353,7 +358,7 @@ begin
       FreeAndNil(FStream);
     end;
   finally
-    //Cleanup;
+    Cleanup;
   end;
 end;
 
