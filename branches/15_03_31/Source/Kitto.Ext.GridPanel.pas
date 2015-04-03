64c9aa4d9b9932aab225e5631462844a0446db58
nando <nando@c6421abf-fb66-e144-b6b4-7ec664d51aad>
2015-04-03 15:47:37 +0000

    * GridPanel: Display labels instead of ids for a column with an AllowedValues node. * GridPanel: In mobile browsers only activate the single touch event if a DefaultAction is explicitly provided.

--- branches/15_03_31/Source/Kitto.Ext.GridPanel.pas
+++ branches/15_03_31/Source/Kitto.Ext.GridPanel.pas
@@ -67,6 +67,7 @@ type
     procedure ShowConfirmButtons(const AShow: Boolean);
     function HasDefaultAction: Boolean;
     function GetDefaultAction: string;
+    function HasExplicitDefaultAction: Boolean;
   strict protected
     procedure ExecuteNamedAction(const AActionName: string); override;
     function GetEditWindowDefaultControllerType: string; virtual;
@@ -364,6 +365,8 @@ var
       LCustomRenderer: TEFNode;
       LColorPairs: TEFPairs;
       LColors: TEFNode;
+      LAllowedValues: TEFPairs;
+      LJSCode: string;
     begin
       Result := False;
 
@@ -425,6 +428,21 @@ var
             'metaData.css += getColorStyleRuleForValue(value, [%s]);' +
             'return %s ? null : formatWithDisplayTemplate(value, ''%s'');',
             [PairsToJSON(LColorPairs), IfThen(AViewField.BlankValue, 'true', 'false'), AViewField.DisplayTemplate]));
+        Result := True;
+        Exit;
+      end;
+
+      LAllowedValues := AViewField.GetChildrenAsPairs('AllowedValues', True);
+      if Length(LAllowedValues) > 0 then
+      begin
+        LJSCode := '';
+        for I := Low(LAllowedValues) to High(LAllowedValues) do
+        begin
+          LAllowedValues[I].Value := _(LAllowedValues[I].Value);
+          LJSCode := LJSCode + Format('if (v == "%s") return "%s";' + sLineBreak, [LAllowedValues[I].Key, LAllowedValues[I].Value]);
+        end;
+        LJSCode := LJSCode + 'return v;';
+        AColumn.RendererExtFunction := AColumn.JSFunction('v', LJSCode);
         Result := True;
         Exit;
       end;
@@ -809,12 +827,15 @@ begin
 
   if not FInplaceEditing and HasDefaultAction then
   begin
-    //if Session.IsMobileBrowser then
-    //  LEventName := 'rowclick'
-    //else
+    if Session.IsMobileBrowser then
+      LEventName := IfThen(HasExplicitDefaultAction, 'rowclick', '')
+    else
       LEventName := 'rowdblclick';
+    if LEventName <> '' then
+    begin
       LKeyFieldNames := Join(LViewTable.GetKeyFieldAliasedNames, ',');
       FEditorGridPanel.On(LEventName, AjaxSelection(DefaultAction, FSelectionModel, LKeyFieldNames, LKeyFieldNames, []));
+    end;
   end;
 
   // By default show paging toolbar for large models.
@@ -862,6 +883,11 @@ begin
   Result := LDefaultAction <> '';
   if not Result then
     Result := FIsActionAllowed[VIEW_OPERATION] or FIsActionAllowed[EDIT_OPERATION];
+end;
+
+function TKExtGridPanel.HasExplicitDefaultAction: Boolean;
+begin
+  Result := GetDefaultAction <> '';
 end;
 
 procedure TKExtGridPanel.CheckGroupColumn;
