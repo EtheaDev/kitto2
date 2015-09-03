282758ed985564243f6a8d8e2bd1f25ed8da8ce5
Carlo Barazzetta <carlo.barazzetta@ethea.it>
2015-09-03 16:46:36 +0000

    *Reverted changes to Grouping/SortFieldNames

--- Examples/HelloKitto/Source/UseKitto.pas
+++ Examples/HelloKitto/Source/UseKitto.pas
@@ -17,7 +17,7 @@ uses
   // Kitto.Auth.DBServer,
   // Kitto.Auth.OSDB,
   // Kitto.Auth.TextFile,
-  Kitto.Ext.ADOTools, //For Excel export
+  Kitto.Ext.ADOTools, //For Excel/Import export
   Kitto.Ext.DebenuQuickPDFTools, //For PDF Merge
   Kitto.Ext.FOPTools, //For FOP Engine
   Kitto.Localization.dxgettext,
