a82bee15121eb7fe78768a1ad23386922ea51fc7
Carlo Barazzetta <carlo.barazzetta@ethea.it>
2017-09-25 17:29:19 +0200

    +DbDemos example

---
+++ Examples/DbDemos/Source/UseKitto.pas
@@ -0,0 +1,19 @@
+unit UseKitto;
+
+interface
+
+uses
+  // EF.DB.ADO,
+  EF.DB.FD,
+  // EF.DB.DBX,
+Kitto.AccessControl.DB,
+  Kitto.Auth.TextFile,
+  Kitto.Localization.dxgettext,
+  Kitto.Metadata.ModelImplementation,
+  Kitto.Metadata.ViewBuilders,
+  Kitto.Ext.All
+  ;
+
+implementation
+
+end.
